a94bea856372159930f0eeca581654b05d18deb4
RHL120 <rhl120@protonmail.com>
2024-01-29 19:11:53 +0100

    Fix the test

--- src/parser.rs
+++ src/parser.rs
@@ -329,7 +329,7 @@ mod tests {
     #[test]
     fn fib_func() {
         assert_eq!(
-            Parser::new(r#"fn fib :: n -> if n <= 1 ? n : fib(n - 1) + fib(n - 2);"#.to_owned())
+            Parser::new(r#"lam fib :: n -> if n <= 1 ? n : fib(n - 1) + fib(n - 2);"#.to_owned())
                 .program()
                 .unwrap(),
             Program(vec![Expr::LamDef {
