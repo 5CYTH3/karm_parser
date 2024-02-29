359e2bd8b3d96d4ed5b8e22d078430be6fb10d20
5CYTH3 <ScytheD@outlook.fr>
2024-03-01 00:05:57 +0100

    Added TODOs

--- src/lexer.rs
+++ src/lexer.rs
@@ -6,6 +6,36 @@ use self::tokens::Kind;
 
 pub mod tokens;
 
+
+// Don't mess up the order or it becomes hell
+const REGEX_SET: Vec<(&str, Option<Kind>)> = vec![
+    (r"^\d+", Some(Kind::Integer)), // Integers
+    (r"^\n", Some(Kind::Newline)),  // Newline
+    (r"^\s+", None),                // Whitespace
+    (r"^\blam\b", Some(Kind::Lam)),
+    (r"^\buse\b", Some(Kind::Use)),
+    (r"^\bif\b", Some(Kind::If)),
+    (r"^::", Some(Kind::DoubleColon)),
+    (r"^:", Some(Kind::Colon)),
+    (r"^;", Some(Kind::SemiColon)),
+    (r"^\|", Some(Kind::Bar)),
+    (r#"^"[^"]*""#, Some(Kind::String)),
+    (r"^->", Some(Kind::Arrow)),
+    (r"^\*", Some(Kind::Mul)),
+    (r"^/", Some(Kind::Div)),
+    (r"^\+", Some(Kind::Plus)),
+    (r"^\-", Some(Kind::Min)),
+    (r"^<=", Some(Kind::Leq)),
+    (r"^>=", Some(Kind::Geq)),
+    (r"^==", Some(Kind::DoubleEq)),
+    (r"^!=", Some(Kind::Neq)),
+    (r"^,", Some(Kind::Comma)),
+    (r"^\?", Some(Kind::QMark)),
+    (r"^\(", Some(Kind::LParen)),
+    (r"^\)", Some(Kind::RParen)),
+    (r"^\w+", Some(Kind::Ident)),
+];
+
 #[derive(Clone)]
 pub struct Lexer {
     program: String,
@@ -28,6 +58,7 @@ impl Lexer {
         self.cursor < self.program.len()
     }
 
+    // TODO: Obviously implement that function to have a more readable code
     fn _match_token(&mut self, _regexp: (&str, Kind), _ctx: &str) {}
 
     pub fn get_next(&mut self) -> Option<Token> {
@@ -35,37 +66,10 @@ impl Lexer {
             return None;
         }
 
-        // Don't mess up the order or it becomes hell
-        let r_set: Vec<(&str, Option<Kind>)> = vec![
-            (r"^\d+", Some(Kind::Integer)), // Integers
-            (r"^\n", Some(Kind::Newline)),  // Newline
-            (r"^\s+", None),                // Whitespace
-            (r"^\blam\b", Some(Kind::Lam)),
-            (r"^\buse\b", Some(Kind::Use)),
-            (r"^\bif\b", Some(Kind::If)),
-            (r"^::", Some(Kind::DoubleColon)),
-            (r"^:", Some(Kind::Colon)),
-            (r"^;", Some(Kind::SemiColon)),
-            (r"^\|", Some(Kind::Bar)),
-            (r#"^"[^"]*""#, Some(Kind::String)),
-            (r"^->", Some(Kind::Arrow)),
-            (r"^\*", Some(Kind::Mul)),
-            (r"^/", Some(Kind::Div)),
-            (r"^\+", Some(Kind::Plus)),
-            (r"^\-", Some(Kind::Min)),
-            (r"^<=", Some(Kind::Leq)),
-            (r"^>=", Some(Kind::Geq)),
-            (r"^==", Some(Kind::DoubleEq)),
-            (r"^!=", Some(Kind::Neq)),
-            (r"^,", Some(Kind::Comma)),
-            (r"^\?", Some(Kind::QMark)),
-            (r"^\(", Some(Kind::LParen)),
-            (r"^\)", Some(Kind::RParen)),
-            (r"^\w+", Some(Kind::Ident)),
-        ];
         // Add a way to detect if the token is in the r_set.
+        // TODO: Maybe improve this function by using a plain iterator and making Lexer implement iterator.
         let s_str = &self.program[self.cursor..];
-        for r_s in r_set {
+        for r_s in REGEX_SET {
             match Regex::new(r_s.0).unwrap().captures(s_str) {
                 Some(caps) => {
                     let capture = caps.get(0).unwrap().as_str();
